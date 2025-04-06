6cee6e5353333acbb44d4a4ea8bcf478edb715e2
Antonio Kim <antonio_kim@outlook.com>
2025-04-06 04:21:37 -0700

    adding text dataset and dataloader

---
+++ src/utils/text_dataloader.py
@@ -0,0 +1,17 @@
+import tiktoken
+from torch.utils.data import DataLoader
+
+from .text_dataset import TextDataset
+
+def text_dataloader(text, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = TextDataset(text, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
---
+++ src/utils/text_dataset.py
@@ -0,0 +1,39 @@
+import torch
+from torch.utils.data import Dataset
+
+class TextDataset(Dataset):
+    """Convert text file(s) into readable data for training in torch
+    
+    This dataset processes a text file into series of chunks for model
+    training. Each chunk consists of max_length token for input and 
+    subsequent token as targets
+
+    Args:
+        text: location of the text file
+        tokenizer: requires in order to convert text into tokens. Tokens
+            are set of integers that is translated from words or part 
+            of words into numbers that the LLM can interpret
+        max_length (int): maximum length of token sequence
+        stride: interval size to slide the chunk window over tokenized
+            text. Stride of 1 means there's no overlap, while higher 
+            stride allows overlapping chunks
+    """
+    def __init__(self, text, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(text)
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i+1: i + max_length + 1]
+
+            # convert to tensor and store them in chunks
+            self.inputs_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    # Return tokenized input and output pair in given index for training
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
